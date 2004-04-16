9f2cee27a7618053f2e7350bed45de017724b851
Laurent Sansonetti <lrz@gnome.org>
2004-04-16 19:26:14 +0000

    2004-04-16  Laurent Sansonetti  <lrz@gnome.org>
            * HACKING:
    
            New file.
    
            * lib/alexandria.rb,
              AUTHORS:
    
            Added Daffyd and Zachary.
    
            * bin/alexandria,
              lib/alexandria.rb,
              lib/alexandria/book.rb,
              lib/alexandria/ui.rb,
              lib/alexandria/ui/about_dialog.rb,
              lib/alexandria/ui/alert_dialog.rb,
              lib/alexandria/ui/glade_base.rb,
              lib/alexandria/ui/icons.rb,
              lib/alexandria/ui/info_book_dialog.rb,
              lib/alexandria/ui/main_app.rb,
              lib/alexandria/ui/new_book_dialog.rb:
    
            Prefixed with GPL header (required by the the license).
    
    
    git-svn-id: http://alexandria.rubyforge.org/svn/trunk/alexandria@71 9992d806-23ff-4cc5-b4c6-abbc17c6a14e

--- lib/alexandria.rb
+++ lib/alexandria.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2004 Laurent Sansonetti
+#
+# Alexandria is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Alexandria is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with Alexandria; see the file COPYING.  If not,
+# write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
 require 'alexandria/config.rb'
 require 'alexandria/book.rb'
 require 'alexandria/ui.rb'
@@ -7,7 +24,11 @@ module Alexandria
     VERSION = '0.1.2'
     DESCRIPTION = 'A program to help you manage your book collection.'
     COPYRIGHT = 'Copyright (C) 2004 Laurent Sansonetti'
-    AUTHORS = [ 'Laurent Sansonetti <lrz@gnome.org>' ]
+    AUTHORS = [
+        'Dafydd Harries <daf@muse.19inch.net>',
+        'Laurent Sansonetti <lrz@gnome.org>',
+        'Zachary P. Landau <kapheine@hypa.net>'
+    ]
     DOCUMENTERS = [ 'Laurent Sansonetti <lrz@gnome.org>' ]  
     TRANSLATORS = [ 'Laurent Sansonetti <lrz@gnome.org>' ]
     LIST = 'alexandria-list@rubyforge.org'
