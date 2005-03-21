a0edd3e3b8bb4209fef9f88f1268b9a0ce4a7d00
Laurent Sansonetti <lrz@gnome.org>
2005-03-21 21:12:08 +0000

    2005-03-21  Laurent Sansonetti  <lrz@gnome.org>
        * data/alexandria/COPYING:
    
        Removed.
    
        * lib/alexandria/ui/about_dialog.rb:
    
        Do not display the whole GPL text, just the header.
    
    
    git-svn-id: http://alexandria.rubyforge.org/svn/trunk/alexandria@308 9992d806-23ff-4cc5-b4c6-abbc17c6a14e

--- lib/alexandria/ui/about_dialog.rb
+++ lib/alexandria/ui/about_dialog.rb
@@ -21,6 +21,23 @@ module UI
         include GetText
         GetText.bindtextdomain(Alexandria::TEXTDOMAIN, nil, nil, "UTF-8")
 
+        GPL = <<EOL
+Alexandria is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+Alexandria is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with Alexandria; see the file COPYING.  If not,
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+EOL
+
         def initialize(parent)
             super()
             self.name = Alexandria::TITLE
@@ -33,8 +50,7 @@ module UI
             self.translator_credits = Alexandria::TRANSLATORS.join("\n")
             self.logo = Icons::ALEXANDRIA
             self.website = Alexandria::WEBSITE_URL
-            self.license = File.read(File.join(Alexandria::Config::DATA_DIR,
-                                               "COPYING"))
+            self.license = GPL
             self.transient_for = parent
             signal_connect('destroy') { hide }
         end
