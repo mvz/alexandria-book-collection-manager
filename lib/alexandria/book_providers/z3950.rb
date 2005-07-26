a4c14b05ec70cfce13a3f15642c95ea499bb3bda
Laurent Sansonetti <lrz@gnome.org>
2005-07-26 22:07:50 +0000

    2005-07-27  Laurent Sansonetti  <lrz@gnome.org>
        * lib/alexandria/book_providers/z3950.rb:
    
        Completed the Z39.50 provider.
    
    
    git-svn-id: http://alexandria.rubyforge.org/svn/trunk/alexandria@436 9992d806-23ff-4cc5-b4c6-abbc17c6a14e

--- lib/alexandria/book_providers/z3950.rb
+++ lib/alexandria/book_providers/z3950.rb
@@ -16,6 +16,7 @@
 # Boston, MA 02111-1307, USA.
 
 require 'zoom'
+require 'zoom/marc'
 
 module Alexandria
 class BookProviders
@@ -26,15 +27,55 @@ class BookProviders
         def initialize
             super("Z3950", "Z39.50")
             prefs.add("hostname", _("Hostname"), "")
-            prefs.add("port", _("Port"), 7070)
+            prefs.add("port", _("Port"), 7090)
             prefs.add("database", _("Database"), "")
             prefs.add("username", _("Username"), "", nil, false)
             prefs.add("password", _("Password"), "", nil, false)
         end
        
         def search(criterion, type)
-            # TODO
             prefs.read
+
+            options = {}
+            unless prefs['username'].empty? or prefs['password'].empty?
+                options['user'] = prefs['username']
+                options['password'] = prefs['password']
+            end
+            hostname, port = prefs['hostname'], prefs['port'].to_i
+            conn = ZOOM::Connection.new(options).connect(hostname, port)
+            conn.database_name = prefs['database']
+            conn.preferred_record_syntax = 'USMARC'
+            attr = case type
+                when SEARCH_BY_ISBN     then 7
+                when SEARCH_BY_TITLE    then 4
+                when SEARCH_BY_AUTHORS  then 1003
+                when SEARCH_BY_KEYWORD  then 1016
+            end
+            pqf = "@attr 1=#{attr} '#{criterion.upcase}'"
+            puts "pqf is #{pqf}" if $DEBUG
+            rset = conn.search(pqf)
+            puts "total #{rset.length}" if $DEBUG
+            raise NoResultsError if rset.length == 0
+            results = []
+            rset[0..10].each do |record|
+                marc = MARC::Record.new(record.render)
+
+                if $DEBUG
+                    puts "title: #{marc.title}"
+                    puts "authors: #{marc.authors.join(', ')}"
+                    puts "isbn: #{marc.isbn}"
+                    puts "publisher: #{marc.publisher}"
+                    puts "edition: #{marc.edition}"
+                end
+
+                next if marc.title.nil? or marc.authors.empty?
+                
+                book = Book.new(marc.title, marc.authors, marc.isbn, 
+                                (marc.publisher or ""), 
+                                (marc.edition or ""))
+                results << [book, nil]
+            end
+            type == SEARCH_BY_ISBN ? results.first : results
         end
     end
 end
