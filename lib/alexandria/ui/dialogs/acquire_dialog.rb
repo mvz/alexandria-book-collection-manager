bae9e305778a06eaf958cdaf9f1f629b48e3e3b4
Cathal Mc Ginley <cathal.alexandria@gnostai.org>
2007-09-18 01:58:48 +0000

    Acquire from Scanner dialog now accepts scans from an unmodified CueCat and allows users to add scanned Books to any Library.
    git-svn-id: http://alexandria.rubyforge.org/svn/trunk/alexandria@720 9992d806-23ff-4cc5-b4c6-abbc17c6a14e

--- lib/alexandria/ui/dialogs/acquire_dialog.rb
+++ lib/alexandria/ui/dialogs/acquire_dialog.rb
@@ -37,14 +37,71 @@ module UI
                 selected_library = libraries.first
             end
             @combo_libraries.populate_with_libraries(libraries,
-                                                     selected_library) 
+                                                     selected_library)
 
-            @add_button.sensitive = false 
+            @add_button.sensitive = false
             setup_scanner_area
             init_treeview
+            @book_results = Hash.new
         end
 
         def on_add
+            model = @barcodes_treeview.model
+            selection = @barcodes_treeview.selection
+            isbns = []
+            if selection.count_selected_rows > 0
+                model.freeze_notify do
+                    # capture isbns
+                    selection.selected_each do |model, path, iter|
+                        isbns << iter[0]
+                    end
+                    # remove list items (complex, cf. tutorial...)
+                    # http://ruby-gnome2.sourceforge.jp/hiki.cgi?tut-treeview-model-remove
+                    #row_refs = []
+                    #paths = selection.selected_rows
+                    #paths.each do |path|
+                    #    row_refs << Gtk::TreeRowReference.new(model, path)
+                    #end
+                    #row_refs.each do |ref|
+                    #    model.remove(model.get_iter(ref.path))
+                    #end
+
+                    # try it this way... works because of persistent iters
+                    row_iters = []
+                    selection.selected_rows.each do |path|
+                        row_iters << model.get_iter(path)
+                    end
+                    row_iters.each do |iter|
+                        model.remove(iter)
+                    end
+
+                end
+            else
+                model.freeze_notify do
+                    # capture isbns
+                    model.each do |model, path, iter|
+                        isbns << iter[0]
+                    end
+                    # remove list items
+                    model.clear
+                end
+            end
+
+            libraries = Libraries.instance.all_libraries
+            library, new_library =
+                @combo_libraries.selection_from_libraries(libraries)
+            isbns.each do |isbn|
+                puts "Adding #{isbn}"
+                result = @book_results[isbn]
+                book = result[0]
+                cover_uri = result[1]
+
+                unless cover_uri.nil?
+                    library.save_cover(book, cover_uri)
+                end
+                library << book
+                library.save(book)
+            end
         end
 
         def on_cancel
@@ -73,23 +130,45 @@ module UI
                 puts "<<< #{isbn} >>>"
                 # TODO :: sound
                 # play_sound("gnometris/turn")
-                
+
                 #t = Thread.new(isbn) do |isbn|
                 @barcodes_treeview.model.freeze_notify do
                     iter = @barcodes_treeview.model.append
                     iter[0] = isbn
                     iter[1] = "<<Title>>"
                 end
+                lookup_book(isbn)
                 #end
             else
                 puts "was not an ISBN barcode"
                 # TODO :: sound
                 # play_sound("question")
-            end 
+            end
         end
 
         private
-        
+
+        def lookup_book(isbn)
+            lookup_thread = Thread.new(isbn) do |isbn|
+                begin
+                    results = Alexandria::BookProviders.isbn_search(isbn)
+                    book = results[0]
+                    @book_results[isbn] = results
+                    @barcodes_treeview.model.freeze_notify do
+                        iter = @barcodes_treeview.model.each do |model, path, iter|
+                            if iter[0] == isbn
+                                iter[1] = book.title
+                                model.row_changed(path, iter)
+                            end
+                        end
+                    end
+                    @add_button.sensitive = true
+                rescue StandardError => err
+                    puts err
+                end
+            end
+        end
+
         def setup_scanner_area
             @scanner_buffer = ""
             @scanner = Alexandria::Scanners::CueCat.new # HACK :: use Registry
@@ -101,23 +180,39 @@ module UI
             @scan_area.signal_connect("focus-in-event") do |widget, event|
                 @barcode_label.label = _("_Barcode Scanner Ready")
                 @scanner_buffer = ""
+                begin
+                    # @frame1.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.new(0, 0, 0xEE))
+                    # @frame1.modify_bg(Gtk::STATE_ACTIVE, Gdk::Color.new(0, 0, 0xEE))
+                    points = [[-100,-10], [300,-10], [300,300], [-100,300]]
+                    @scanner_background = Gnome::CanvasPolygon.new(@barcode_canvas.root,
+                                          {:points => points, :fill_color_rgba => 0xFDFDFDFF})
+                rescue StandardError => err
+                    puts err
+                end
             end
             @scan_area.signal_connect("focus-out-event") do |widget, event|
-                @barcode_label.label = _("Click Here To Scan _Barcodes")
+                @barcode_label.label = _("Click below to scan _barcodes")
                 @scanner_buffer = ""
+                @scanner_background.destroy
             end
 
+            @@debug_index = 0
             @scan_area.signal_connect("key-press-event") do |button, event|
                 #puts event.keyval
                 if event.keyval < 255
                     if @scanner_buffer.empty?
-                        # this is our first character, notify user
-                        puts "Scanning... "                     
+                        if event.keyval.chr == '`' # backtick key for devs
+                            developer_test_scan
+                            next
+                        else
+                            # this is our first character, notify user
+                            puts "Scanning... "
+                        end
                         # TODO :: sound
                         # play_sound("iagno/flip-piece")
                     end
                     @scanner_buffer << event.keyval.chr
-                    
+
                     # or get event.keyval == 65293 meaning Enter key
                     if @scanner.match? @scanner_buffer
                         read_barcode_scan
@@ -131,9 +226,27 @@ module UI
 
         end
 
+        def developer_test_scan
+            puts "Developer test scan..."
+            scans = [".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3j3C3f1Dxj3Dq.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3z0CNj3Dhj1EW.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3r2DNbXCxTZCW.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGf2.ENr7C3z0DNn0ENnWE3nZDhP6.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7CNT2CxT2ChP0Dq.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7CNT6E3f7CNbWDa.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3b3ENjYDxv3EW.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3b2DxjZE3b3Dq.",
+                     ".C3nZC3nZC3n2ChnWENz7DxnY.cGen.ENr7C3n6CNr6DxvYDa."]
+            @scanner_buffer = scans[@@debug_index % scans.size]
+            @@debug_index += 1
+            read_barcode_scan
+        end
+
         def init_treeview
             puts 'initializing treeview...'
             liststore = Gtk::ListStore.new(String, String)
+
+            @barcodes_treeview.selection.mode = Gtk::SELECTION_MULTIPLE
 
             @barcodes_treeview.model = liststore
 
@@ -147,10 +260,17 @@ module UI
             # Add column using the second renderer
             col = Gtk::TreeViewColumn.new("Title", text_renderer, :text => 1)
             @barcodes_treeview.append_column(col)
+
+
+            @barcodes_treeview.model.signal_connect("row-deleted") do |model, path|
+                if not model.iter_first
+                    @add_button.sensitive = false
+                end
+            end
         end
-        
+
         #def play_sound(filename)
-        #    dir = "/usr/share/sounds"        
+        #    dir = "/usr/share/sounds"
         #    Gnome::Sound.play("#{dir}/#{filename}.wav")
         #end
 
