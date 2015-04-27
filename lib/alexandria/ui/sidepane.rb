001512c01978da3aa892c903086e2858180cdf1d
Matijs van Zuijlen <matijs@matijs.net>
2015-04-27 17:10:33 +0200

    Add full license header

--- lib/alexandria/ui/sidepane.rb
+++ lib/alexandria/ui/sidepane.rb
@@ -1,4 +1,22 @@
-# Copyright (C) 2011 Matijs van Zuijlen
+# Copyright (C) 2008 Joseph Method
+# Copyright (C) 2008,2009 Cathal Mc Ginley
+# Copyright (C) 2011,2014,2015 Matijs van Zuijlen
+#
+# Alexandria is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Alexandria is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with Alexandria; see the file COPYING.  If not,
+# write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301 USA.
+
 module Alexandria
   module UI
     class SidePaneManager
