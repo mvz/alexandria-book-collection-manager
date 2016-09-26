d05ea80be2948cf4957bcea7c19f5cfa2442b8c9
Matijs van Zuijlen <matijs@matijs.net>
2016-09-26 13:34:47 +0200

    Spec ui/sound

---
+++ spec/alexandria/ui/sound_spec.rb
@@ -0,0 +1,25 @@
+# Copyright (C) 2015 Matijs van Zuijlen
+#
+# Alexandria is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Alexandria is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with Alexandria; see the file COPYING.  If not,
+# write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301 USA.
+
+require 'spec_helper'
+
+describe Alexandria::UI::SoundEffectsPlayer do
+  it 'can be instantiated' do
+    player = described_class.new
+    expect(player).to be_instance_of described_class
+  end
+end
