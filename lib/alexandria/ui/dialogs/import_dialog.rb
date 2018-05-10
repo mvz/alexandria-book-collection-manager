f555e4c5ca00cdb16db32ad4d586c4491170ec99
Matijs van Zuijlen <matijs@matijs.net>
2018-05-10 17:52:55 +0200

    Fix copyright headers

--- lib/alexandria/ui/dialogs/import_dialog.rb
+++ lib/alexandria/ui/dialogs/import_dialog.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This file is part of the Alexandria build system.
+# This file is part of Alexandria.
 #
 # See the file README.md for authorship and licensing information.
 
