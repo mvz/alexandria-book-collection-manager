7e658b81878fcaafd131085fa425494ab3a5462e
Matijs van Zuijlen <matijs@matijs.net>
2018-09-23 13:29:09 +0200

    Fix use of Gtk::Stock

--- lib/alexandria/ui/error_dialog.rb
+++ lib/alexandria/ui/error_dialog.rb
@@ -10,8 +10,8 @@ module Alexandria
   module UI
     class ErrorDialog < AlertDialog
       def initialize(parent, title, message = nil)
-        super(parent, title, Gtk::Stock::DIALOG_ERROR,
-              [[Gtk::Stock::OK, :ok]], message)
+        super(parent, title, Gtk::STOCK_DIALOG_ERROR,
+              [[Gtk::STOCK_OK, :ok]], message)
         # FIXME: Should accept just :ok
         dialog.default_response = Gtk::ResponseType::OK
       end
