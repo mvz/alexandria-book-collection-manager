af25734a63f1ebf2753557526d822b4b2b2ec27c
Matijs van Zuijlen <matijs@matijs.net>
2018-09-27 11:25:48 +0200

    WIP: Port to GirFFI

--- lib/alexandria/ui/import_dialog.rb
+++ lib/alexandria/ui/import_dialog.rb
@@ -21,7 +21,7 @@ module Alexandria
 
       def initialize(parent)
         title = _("Import a Library")
-        @dialog = Gtk::FileChooserDialog.new title: title, parent: parent, action: :open
+        @dialog = Gtk::FileChooserDialog.new title, parent, :open, []
         log.debug { "ImportDialog opened" }
         @destroyed = false
         @running = false
