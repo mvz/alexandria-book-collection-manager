4115135ff0ac21065f31451cdf0770047cddb732
Matijs van Zuijlen <matijs@matijs.net>
2020-05-24 17:26:12 +0200

    Make ImportDialog not be a SimpleDelegator

--- spec/alexandria/ui/import_dialog_spec.rb
+++ spec/alexandria/ui/import_dialog_spec.rb
@@ -7,8 +7,18 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Alexandria::UI::ImportDialog do
-  it "works" do
-    parent = Gtk::Window.new :toplevel
+  let(:parent) { Gtk::Window.new :toplevel }
+
+  it "can be instantiated" do
     described_class.new parent
+  end
+
+  describe "#acquire" do
+    it "works when response is cancel" do
+      import_dialog = described_class.new parent
+      chooser = import_dialog.dialog
+      allow(chooser).to receive(:run).and_return(Gtk::ResponseType::CANCEL)
+      import_dialog.acquire
+    end
   end
 end
