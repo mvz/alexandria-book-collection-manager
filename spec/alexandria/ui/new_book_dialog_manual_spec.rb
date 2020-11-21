b94b469fbe7288e1dd04bf583ed77b1a090ac5a0
Matijs van Zuijlen <matijs@matijs.net>
2020-11-21 17:39:01 +0100

    Add spec for BookPropertiesDialog#on_change_cover

--- spec/alexandria/ui/new_book_dialog_manual_spec.rb
+++ spec/alexandria/ui/new_book_dialog_manual_spec.rb
@@ -7,6 +7,16 @@
 require_relative "../../spec_helper"
 
 describe Alexandria::UI::NewBookDialogManual do
+  let(:parent) { Gtk::Window.new :toplevel }
+  let(:library) {
+    store = Alexandria::LibraryCollection.instance.library_store
+    store.load_library("Bar Library")
+  }
+  let(:book) do
+    Alexandria::Book.new("Foo Book", ["Jane Doe"], "98765432", "Bar Publisher",
+                         1972, "edition")
+  end
+
   it "works" do
     parent = Gtk::Window.new :toplevel
     library = instance_double(Alexandria::Library)
