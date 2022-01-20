556b06890dea7f9dcd29580311ec5a3c9a963f87
Matijs van Zuijlen <matijs@matijs.net>
2022-01-20 07:59:32 +0100

    Extract BLProvider to its own file

---
+++ spec/alexandria/book_providers/bl_provider_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# This file is part of Alexandria.
+#
+# See the file README.md for authorship and licensing information.
+
+require "spec_helper"
+
+describe Alexandria::BookProviders::BLProvider do
+  it "works" do
+    assert_correct_search_result(described_class, "9781853260803")
+  end
+end
--- spec/alexandria/book_providers_spec.rb
+++ spec/alexandria/book_providers_spec.rb
@@ -9,12 +9,6 @@ require "spec_helper"
 describe Alexandria::BookProviders do
   it "should be less clever"
 
-  describe Alexandria::BookProviders::BLProvider do
-    it "works" do
-      assert_correct_search_result(described_class, "9781853260803")
-    end
-  end
-
   describe Alexandria::BookProviders::SBNProvider do
     it "works" do
       assert_correct_search_result(described_class, "9788835926436")
