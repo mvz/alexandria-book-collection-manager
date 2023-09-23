690e7df2bc7bd4ec6da808b1e74bd8286920bbf7
Matijs van Zuijlen <matijs@matijs.net>
2023-09-23 11:09:21 +0200

    Move Nokogiri memory management setting into the code
    We always want to configure things this way, not just in CI.

--- lib/alexandria/book_providers/website_based_provider.rb
+++ lib/alexandria/book_providers/website_based_provider.rb
@@ -4,6 +4,25 @@
 #
 # See the file README.md for authorship and licensing information.
 
+# By default, Nokogiri will configure libxml2 to use Ruby's memory
+# management functions. This leads to the following sequence of events:
+#
+# - GC starts
+# - Ruby/GNOME traverses the known GObject objects
+# - Ruby/GNOME fetches properties for all objects
+# - One of the properties (I do not know which one) gets initialized to its default value
+# - As part of this process some SVG is parsed
+# - Do do this, some memory needs to be allocated
+# - Ruby does not want memory to be allocated during GC
+# - The process crashes
+#
+# To avoid this, before loading nokogiri, set an environment variable that
+# tells Nokogiri to leave libxml2's memory management alone.
+#
+# See https://github.com/sparklemotion/nokogiri/blob/main/adr/2023-04-libxml-memory-management.md
+#
+ENV["NOKOGIRI_LIBXML_MEMORY_MANAGEMENT"] = "default"
+
 require "nokogiri"
 require "htmlentities"
 
