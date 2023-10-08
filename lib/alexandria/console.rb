19c23db799b371d92ecb56551f8424253338cbf8
Matijs van Zuijlen <matijs@matijs.net>
2023-10-08 12:18:37 +0200

    Move code in alexandria/console into a separate module

--- lib/alexandria/console.rb
+++ lib/alexandria/console.rb
@@ -5,16 +5,18 @@
 # See the file README.md for authorship and licensing information.
 
 module Alexandria
-  def self.list_books_on_console
-    collection = Alexandria::LibraryCollection.instance
-    collection.reload
-    libraries = collection.all_regular_libraries
-    output_string = ""
-    @books = libraries.flatten
-    @books.each do |book|
-      book_authors = book.authors.join(" & ")
-      output_string += [book.title, book_authors].join(", ") + "\n"
+  module Console
+    def self.list_books_on_console
+      collection = Alexandria::LibraryCollection.instance
+      collection.reload
+      libraries = collection.all_regular_libraries
+      output_string = ""
+      @books = libraries.flatten
+      @books.each do |book|
+        book_authors = book.authors.join(" & ")
+        output_string += [book.title, book_authors].join(", ") + "\n"
+      end
+      output_string
     end
-    output_string
   end
 end
